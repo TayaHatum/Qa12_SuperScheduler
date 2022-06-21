c21f1b0b6d132339e1389d301b66399494f6a867
TayaHatum <21taya21@gmail.com>
2022-06-21 19:41:00 +0300

    added config

---
+++ Qa12_SuperScheduler/src/test/java/tests/LaunchTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import manager.Configuration;
+import org.testng.annotations.Test;
+
+public class LaunchTest extends Configuration {
+
+    @Test
+    public void launchTest(){
+
+    }
+}
